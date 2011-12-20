0f7bc3f5e9df8968af1f4ee23619b614f35387bb
unknown <Ben Laptop@BenLaptop-HP.(none)>
2011-12-20 14:41:36 +0000

    Worked in test setup

---
+++ Source/WingsOfLiberty.Tests/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace WingsOfLiberty.Tests
+{
+    public class TestBase
+    {
+        public void Describes(string description)
+        {
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine(description);
+            Console.WriteLine("--------------------------------------");
+        }
+
+        public void IsPending()
+        {
+            Console.WriteLine(" -- PENDING -- ");
+            Assert.Inconclusive();
+        }
+
+        public string GetCaller()
+        {
+            StackTrace stack = new StackTrace();
+            return stack.GetFrame(2).GetMethod().Name.Replace("_", " ");
+        }
+    }
+}
