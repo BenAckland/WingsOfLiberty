fa00de732849fb7ae31603e7756b1757899ae82d
unknown <Ben Laptop@BenLaptop-HP.(none)>
2011-12-21 00:03:35 +0000

    Added Moq and played with Nunit

--- Source/WingsOfLiberty.Tests/NUnitSampleTests.cs
+++ Source/WingsOfLiberty.Tests/NUnitSampleTests.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 namespace WingsOfLiberty.Tests {
     [TestFixture]
-    public class NUnitSampleTests {
+    public class NUnitSampleTests : TestBase {
         [Test]
         public void SomePassingTest() {
             Assert.AreEqual(5, 5);
